bf7cf859febf29462245ee1a302a56c843142dea
Aaron32365 <aaronsphone32365@gmail.com>
2021-01-24 07:45:42 +0000

    modified grid system for scalability, increased GUI basic functionality

--- __Main__.py
+++ __Main__.py
@@ -15,20 +15,55 @@ class Application(tk.Frame):
         
         #adjusting outer most GUI layout and options
         self.master.title("FireGuard Test")
-        self.master.configure(background="#F4F6F6")
+        self.master.configure(background="#F5F5F5")
         self.master.attributes("-fullscreen",True)
+        #self.master.rowconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,8,19,20),weight=1)
+        #self.master.columnconfigure(0, weight=1)
 
+        #create 5 col/6 row grid
+        self.master.columnconfigure((0,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24), weight=1)
+        self.master.rowconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18), weight=1)
         #create basic widgets
         self.create_widgets()
         
+        
     #Function for creating baseline widgets
     def create_widgets(self):
-        self.exit_btn = tk.Button(master=self.master, text="Exit", fg="red",
-                                  command=self.master.destroy).grid(row=0,column=1)
-        self.test_sect = tk.Frame(master=self.master, relief="raised",bd=2,bg="#FFFFFF",
-                                  width=250,height=8000)#height of 8k - unsure of how to dynamically fit height
-        self.test_sect.grid(row=0, column=0,rowspan=800)
-       
+
+        #Section for holding the created tests
+        self.test_sect = tk.Frame(master=self.master, relief="groove",bd=2,bg="#AEE8B6")
+        self.test_sect.grid(row=0, column=0, rowspan=20, columnspan=1, sticky="NSEW")
+        self.test_sect.columnconfigure((0,1,2,3,4),weight=1)
+        
+        self.test_sect_header = tk.Label(master=self.test_sect, text="Template Title", bg="#FFFFFF",relief="raised", width=14)
+        self.test_sect_header.grid(row=1, column=2,pady=100,ipady=40)
+        self.test_sect_header.configure(font=("Stencil", 16, "bold"))
+
+        self.header_sect = tk.Frame(master=self.master, relief="raised", bg="#FFFFFF", bd=4)
+        self.header_sect.rowconfigure((0,1,2),weight=1)
+        self.header_sect.columnconfigure((0,1,2),weight=1)
+        self.header_sect.grid(row=2, column=12,rowspan=2,sticky="NSEW")
+        
+        self.header_sect_label = tk.Label(master=self.header_sect, text="No Test Selected", justify="center")
+        self.header_sect_label.configure(font=("Arial", 40, "bold"),bg="#FFFFFF",width=20)
+        self.header_sect_label.grid(column=1,row=1)
+
+        self.body_sect = tk.Frame(master=self.master, relief="groove",bg="#FFFFFF", bd=4)
+        self.body_sect.rowconfigure((0,1,2,3,4), weight=1)
+        self.body_sect.columnconfigure((0,1,2,3,4), weight=1)
+        self.body_sect.grid(row=5, column=6, sticky="NSEW", columnspan=13, rowspan=10)
+
+        self.body_sect_default_label = tk.Label(master=self.body_sect, text="Please select a test to begin.", justify="center")
+        self.body_sect_default_label.configure(font=("Times New Roman",20, "bold"),bg="#FFFFFF")
+        self.body_sect_default_label.grid(row=0, column=2, rowspan=2)
+
+        #self.body_sect_start_btn = tk.Button(master=self.body_sect, text="Start",)
+
+        
+        
+        #self.header_sect.grid_propagate(0)
+        #self.header_sect.grid(row=2, column=1, padx=40, columnspan=15)
+
     #Function for adding seperate tests to main application
     def add_test(self, test):
         self.test_list.append(test)
@@ -38,23 +73,31 @@ class Application(tk.Frame):
         for i in range(len(self.test_list)):
             print(self.test_list[i])
             #grid the test class buttons intervaled with i
-            self.test_list[i].test_btn.grid(row=18+i+i+i+i+i+i,column=0)#this works for setting interval distances between tests - find better solution later
-            pass
+            self.test_list[i].test_btn.grid(row=5+i,column=2, pady=24)#this works for setting interval distances between tests - find better solution later
 
     #Function for choosing the test to take
     def selection(self,test):
         self.selected_test = test
         print(test.test_name)
         print(self.selected_test)
-        pass
+        question = self.questions.get_head_question()
+        print(question.get_next_question().get_question())
+        self.set_test_header()
+        self.set_test_body()
 
+    def set_test_header(self):
+        self.header_sect_label.config(text=self.selected_test.test_name)
+
+    def set_test_body(self):
+        self.body_sect_default_label.destroy()
+        
 #class for creating instances of different tests to add to application
 class Test():
     def __init__(self, parent, questions=None, answers=None, test_name=None):
         self.parent=parent
         self.test_name=test_name
-        self.test_btn = tk.Button(master=self.parent.master, bg="#88F19E", fg="white",padx=24,#make buttons more visually appealing
-                                  pady=10, bd=2, relief='groove',text=self.test_name)
+        self.test_btn = tk.Button(master=self.parent.test_sect, bg="#FFFFFF", fg="black",padx=24,#make buttons more visually appealing
+                                  pady=14, bd=2, relief='raised',text=self.test_name, height=1, width=8)
         self.test_btn.configure(font=("Roboto", 12, "bold"),command=lambda: self.parent.selection(self))#tell Application which test is being selected
         self.questions = questions
         self.answers = answers
@@ -64,10 +107,30 @@ class Test():
 #class for maintaining a given tests questions order
 class Questions_List():
     def __init__(self, question):
-        self.question = question
-        self.next_question = next_question
-        pass
+        self.head_question = question
+        
+    def get_head_question(self):
+        return self.head_question
+    
+    def insert_begenning(self, question):
+        new_question = question
+        new_question.set_next_question(self.head_question)
+        self.head_question = new_question
 
+    def remove_question(self, question_to_remove):
+        current_question = self.get_head_question()
+        if current_question.get_question() == current_question:
+            self.head_question = current_question.get_next_question()
+        else:
+            while current_question:
+                next_question = current_question.get_next_question()
+                if next_question.get_value() == question_to_remove:
+                    current_question.set_next_question(next_question.get_next_question())
+                    current_question = None
+                else:
+                    current_question = next_quetion
+
+    
 #A Node class that will connect questions in a test, as well as hold the question data 
 class Question():
     def __init__(self, package, next_question=None):
@@ -75,26 +138,46 @@ class Question():
         self.next_question = next_question
         pass
 
+    def set_next_question(self, next_question):
+        self.next_question = next_question
+
+    def get_next_question(self):
+        return self.next_question
+
+    def get_question(self):
+        return self.package
 # Main functin for initializing the application and executing primary statements
 
+package1 = {"question": "How are you today?",
+           "choices": {"A": "good", "B": "bad", "C": "Okay"},
+           "answer": "A"}
+
+package2 = {"question2": "How are you today?",
+           "choices": {"A": "good", "B": "bad", "C": "Okay"},
+           "answer": "A"}
 def main():
     
     #instance of Application running mainloop
     app = Application(master=root)
-
+    question_1 = Question(package1)
+    question_2 = Question(package2)
+    question_list_1 = Questions_List(question_1)
+    question_list_1.insert_begenning(question_2)
     
     #testing
     t1q = ["q1","q2"]
     t1a = ["a1","a2"]
-    test1 = Test(app, t1q, t1a, "test1 name")
+    test1 = Test(app, t1q, t1a, "Test 1")
     t2q = ["q3","q4"]
     t2a = ["a3", "a4"]
-    test2 = Test(app, t2q, t2a, "test2 name")
+    test2 = Test(app, t2q, t2a, "Test 2")
+    app.questions = question_list_1
     #testing
     app.add_test(test1)
     app.add_test(test2)
     
     app.generate_tests()
+    
     app.mainloop()
 
 main()
